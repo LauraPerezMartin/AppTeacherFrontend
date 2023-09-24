b616ebccadbe70002b7f5147c182161171d68e6d
DESKTOP-B80RQL8\Laura <laurap48@hotmail.com>
2023-09-24 19:15:14 +0200

    Creación de componentes footer, header, c404, home, mapa y profesor card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { C404Component } from './components/c404/c404.component';
+import { HomeComponent } from './components/home/home.component';
+import { MapaComponent } from './components/home/mapa/mapa.component';
+import { ProfesorCardComponent } from './components/home/profesor-card/profesor-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    C404Component,
+    HomeComponent,
+    MapaComponent,
+    ProfesorCardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/home/profesor-card/profesor-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profesor-card',
+  templateUrl: './profesor-card.component.html',
+  styleUrls: ['./profesor-card.component.css']
+})
+export class ProfesorCardComponent {
+
+}
