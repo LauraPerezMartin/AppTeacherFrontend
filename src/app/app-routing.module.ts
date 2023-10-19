5a932da71629725f0d44176086e3798d5b155b6b
DESKTOP-B80RQL8\Laura <laurap48@hotmail.com>
2023-10-19 20:10:06 +0200

    Cambio de rutas y añadir los componentes mapa y profesor-list a home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { HomeComponent } from './components/home/home.component';
 import { C404Component } from './components/c404/c404.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', component: HomeComponent },
-  { path: 'inicio', component: HomeComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: HomeComponent },
   { path: '**', component: C404Component }
 ];
 
