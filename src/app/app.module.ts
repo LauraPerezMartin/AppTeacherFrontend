6a7b8511202d600823cdd6d9541c7522b32f8c8c
DESKTOP-B80RQL8\Laura <laurap48@hotmail.com>
2023-10-19 20:08:25 +0200

    Importar ProfesorListComponent, ReactiveFormsModule y HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +11,7 @@ import { C404Component } from './components/c404/c404.component';
 import { HomeComponent } from './components/home/home.component';
 import { MapaComponent } from './components/home/mapa/mapa.component';
 import { ProfesorCardComponent } from './components/home/profesor-card/profesor-card.component';
+import { ProfesorListComponent } from './components/home/profesor-list/profesor-list.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +21,14 @@ import { ProfesorCardComponent } from './components/home/profesor-card/profesor-
     C404Component,
     HomeComponent,
     MapaComponent,
-    ProfesorCardComponent
+    ProfesorCardComponent,
+    ProfesorListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
