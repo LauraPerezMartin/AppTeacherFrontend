1a50bc9427ec79c106d0eb9673ad8437b6e29f44
DESKTOP-B80RQL8\Laura <laurap48@hotmail.com>
2023-10-22 19:49:57 +0200

    Quitamos la llamada al servicio para obtenerel listado desde el componente home, obtenemos con input el listado  y hacemose el html del listado

--- src/app/components/home/profesor-list/profesor-list.component.ts
+++ src/app/components/home/profesor-list/profesor-list.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Public } from 'src/app/interfaces/public.interface';
-import { PublicService } from 'src/app/services/public.service';
 
 @Component({
   selector: 'app-profesor-list',
@@ -8,19 +7,5 @@ import { PublicService } from 'src/app/services/public.service';
   styleUrls: ['./profesor-list.component.css']
 })
 export class ProfesorListComponent {
-  profesoresPublic: Public[] = [];
-
-  constructor(
-    private publicService: PublicService
-  ) { }
-
-  async ngOnInit(): Promise<void> {
-    try {
-      let response = await this.publicService.getAll();
-      this.profesoresPublic = response;
-    } catch (error: any) { //comprobar en 1h 3min
-      console.log(error);
-    }
-  }
+  @Input() profesores: Public | any;
 }
-//continuar video 1:09 
