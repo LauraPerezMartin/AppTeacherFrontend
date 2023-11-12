f7e3f5bb87c6873270eba698ba9df1a9ddfa2404
DESKTOP-B80RQL8\Laura <laurap48@hotmail.com>
2023-11-12 19:06:48 +0100

    Obtener la ciudad y/o asignaturas y hacer las busquedas en profesores

--- src/app/components/home/profesor-list/profesor-list.component.ts
+++ src/app/components/home/profesor-list/profesor-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Public } from 'src/app/interfaces/public.interface';
+import { PublicService } from 'src/app/services/public.service';
 
 @Component({
   selector: 'app-profesor-list',
@@ -7,5 +8,40 @@ import { Public } from 'src/app/interfaces/public.interface';
   styleUrls: ['./profesor-list.component.css']
 })
 export class ProfesorListComponent {
-  @Input() profesores: Public | any;
+  profesores: Public[] = [];
+  listadoProfesores: Public[] = [];
+
+  constructor(private publicService: PublicService) { }
+
+  async ngOnInit(): Promise<void> {
+    try {
+      let response = await this.publicService.getAll();
+      this.profesores = response;
+      this.listadoProfesores = this.profesores;
+    } catch (error: any) {
+      console.log(error);
+    }
+  }
+
+  searchProfesores($event: any): void {
+    let busquedaProfesores = this.profesores;
+    if ($event.ciudad) {
+      let result = busquedaProfesores.filter((profesor: Public) => profesor.ciudad === $event.ciudad);
+      busquedaProfesores = result;
+    }
+
+    if ($event.asignatura) {
+      let result: Public[] = [];
+      busquedaProfesores.forEach((profesor: Public) => {
+        let existe = profesor.asignaturas.find((asignatura: any) => asignatura.asignatura_id
+          == $event.asignatura);
+        if (existe) {
+          result.push(profesor);
+        }
+      })
+      busquedaProfesores = result;
+    }
+    this.listadoProfesores = busquedaProfesores;
+  }
 }
+
