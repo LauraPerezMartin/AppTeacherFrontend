75d4f7b951e2c56250570e102ecabdc028d94972
DESKTOP-B80RQL8\Laura <laurap48@hotmail.com>
2023-12-11 20:27:54 +0100

    Creamos el interface de usuario

---
+++ src/app/interfaces/usuario.interface.ts
@@ -0,0 +1,21 @@
+export interface Usuario {
+    id?: number;
+    nombre: string;
+    apellidos: string;
+    username: string;
+    email: string;
+    password: string;
+    telefono: string;
+    direccion: string;
+    ciudad: string;
+    latitud?: string;
+    longitud?: string;
+    imagen?: string;
+    edad: number;
+    fecha_nacimiento: string;
+    genero: string;
+    dni: string;
+    fecha_alta?: string;
+    rol: string;
+    borrado: boolean;
+}
