e4cbf31dd91470929ab7ba1e13f13bb2598dafad
laurap48@hotmail.com <laurap48@hotmail.com>
2024-01-10 19:27:44 +0100

    Añadimos las cabeceras a las cabeceras en las peticiones en el servicio usuarios

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Usuario } from '../interfaces/usuario.interface';
@@ -11,12 +11,18 @@ export class UsuariosService {
   constructor(private httpClient: HttpClient) { }
 
   login(dataUser: any): Promise<any> {
-    return lastValueFrom(this.httpClient.post<any>(`${this.baseUrl}login`, dataUser));
+    return lastValueFrom(this.httpClient.post<any>(`${this.baseUrl}login`, dataUser, this.getHeaders()));
   }
 
   registro(usuario: Usuario): Promise<Usuario> {
-
-    return lastValueFrom(this.httpClient.post<Usuario>(`${this.baseUrl}registro`, usuario))
+    return lastValueFrom(this.httpClient.post<Usuario>(`${this.baseUrl}registro`, usuario, this.getHeaders()));
   }
 
+  getHeaders() {
+    return {
+      headers: new HttpHeaders({
+        "Content-type": "application/json",
+      })
+    }
+  }
 }
