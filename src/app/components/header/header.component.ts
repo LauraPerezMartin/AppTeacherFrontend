0f2ab4430632ae55d2583b6254061be5fd3de123
laurap48@hotmail.com <laurap48@hotmail.com>
2024-01-10 19:34:39 +0100

    Cambios en el menú, comprobación de token y logout en el header.ts

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private router: Router) { }
+  logado: boolean = false;
 
+  ngDoCheck(): void {
+    const token: string | null = localStorage.getItem('mytoken');
+    this.logado = (token) ? true : false;
+  }
+
+  logout(): void {
+    localStorage.removeItem('mytoken');
+    this.router.navigate(['/login']);
+  }
 }
