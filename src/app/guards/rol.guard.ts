5052ad619516ae828e0c133b55e31f748c0594c8
laurap48@hotmail.com <laurap48@hotmail.com>
2024-01-17 19:54:59 +0100

    Creacion de guard para controlar rol de usuario

---
+++ src/app/guards/rol.guard.ts
@@ -0,0 +1,18 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+
+export const rolGuard: CanActivateFn = (route, state) => {
+  const router: Router = inject(Router);
+  const rol: string | null = localStorage.getItem('rol');
+  //const urlRol = route.url.pop()?.toString();
+  const urlRol = route.url.find(urlSeg => urlSeg.toString() == rol);
+  //console.log(`urlSeg: ${urlRol}`);
+  if (rol && urlRol?.toString() === rol) {
+    return true;
+  } else {
+    localStorage.removeItem('mytoken');
+    localStorage.removeItem('rol');
+    router.navigate(['/login']);
+    return false;
+  }
+};
